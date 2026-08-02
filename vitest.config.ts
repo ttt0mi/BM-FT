f700cc94337e7b0d63875555dab56c7b17e9cb3d
tomi omolokun <tonmide96@gmail.com>
2026-08-02 20:59:26 +0100

    refactor(config): typescript config restructure * split typescript config into build and test versions, with tsconfig.json as their shared base configurations * added new build and typechecking scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
+    },
     test: {
         globals: true, // to use describe/it/expect without imports
-        environment: "node",
+        environment: 'node',
+        typecheck: {
+            tsconfig: './tsconfig.test.json',
+        },
         coverage: {
-            reporter: ["text", "html"],
-            exclude: ["node_modules/", "dist/", "prisma/"],
+            reporter: ['text', 'html'],
+            exclude: ['node_modules/', 'dist/', 'prisma/'],
         },
     },
 });
