ea69d80b8d2a7fb930d3fd1d7a75b0de3e518fe5
tomi omolokun <tonmide96@gmail.com>
2026-08-02 02:16:09 +0100

    Corrected import statement for generated PrismaClient

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,6 +1,5 @@
 import "fastify";
-import { type PrismaClient } from "@prisma/client/extension";
-
+import type { PrismaClient } from '@/generated/prisma/client.js';
 declare module "fastify" {
     interface FastifyInstance {
         authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
