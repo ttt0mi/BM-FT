e3511fdb19f556a80d4a00ea920e5c3d24d5a4d7
tomi omolokun <tonmide96@gmail.com>
2026-08-02 02:15:03 +0100

    Improved zod package import statement

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import * as z from 'zod';
 
 export const environmentSchema = z.object({
     NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
