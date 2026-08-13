ced0e379c37d1d4ffa070a8eea951b1ee0cab2e4
tomi omolokun <tonmide96@gmail.com>
2026-08-13 16:57:44 +0100

    fix(vitest): add exclude patterns for test configuration to stop build files from being tested

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     test: {
         globals: true, // to use describe/it/expect without imports
         environment: 'node',
+        exclude: ['**/node_modules/**', '**/dist/**'],
         typecheck: {
             tsconfig: './tsconfig.test.json',
         },
